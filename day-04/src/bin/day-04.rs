1e5b1b6a3d978ccb1ccb2cec8602a11a9af02868
Rob Fisher <robfisher@gmail.com>
2023-12-06 00:28:59 +0000

    Day 4 part 2.

--- day-04/src/bin/day-04.rs
+++ day-04/src/bin/day-04.rs
@@ -1,15 +1,15 @@
 use std::str::FromStr;
 
-
 fn main() {
     let input = include_str!("../input.txt");
     println!("Part 1: {}", part1(input));
+    println!("Part 2: {}", part2(input));
 }
 
 
 #[derive(Debug)]
 struct GameCard {
-    _game_id: u32,
+    game_id: usize,
     winning_numbers: Vec<u32>,
     your_numbers: Vec<u32>,
 }
@@ -40,7 +40,7 @@ impl FromStr for GameCard {
         if let Some(header_section) = sections.next() {
             let header_parts = header_section.split(' ');
             if let Some(game_id_str) = header_parts.last() {
-                if let Ok(game_id) = game_id_str.parse::<u32>() {
+                if let Ok(game_id) = game_id_str.parse::<usize>() {
                     if let Some(number_lists_section) = sections.next() {
                         let mut number_lists = number_lists_section.split('|');
                         if let Some(winning_numbers_str) = number_lists.next() {
@@ -48,7 +48,7 @@ impl FromStr for GameCard {
                                 if let Some(your_numbers_str) = number_lists.next() {
                                     if let Some(your_numbers) = parse_number_list_str(your_numbers_str) {
                                         result = Ok(GameCard {
-                                            _game_id: game_id,
+                                            game_id: game_id,
                                             winning_numbers: winning_numbers,
                                             your_numbers: your_numbers
                                         })
@@ -66,18 +66,34 @@ impl FromStr for GameCard {
 
 
 impl GameCard {
-    fn points(&self) -> u32 {
+    fn score(&self) -> (u32, usize) {
         let mut points = 0u32;
         let mut next_score = 1u32;
+        let mut matching_numbers = 0;
         for my_number in &self.your_numbers {
             for winning_number in &self.winning_numbers {
                 if my_number == winning_number {
                     points = next_score;
                     next_score *= 2;
+                    matching_numbers += 1;
                 }
             }
         }
-        points
+        (points, matching_numbers)
+    }
+
+    fn num_cards_won(&self, game_card_table: &Vec<GameCard>) -> usize {
+        let mut cards_won = 0;
+        let matching_numbers = self.score().1;
+        //println!("card {} has {} matches", self.game_id, matching_numbers);
+        cards_won += matching_numbers;
+        // recursively add the cards won by the copied cards
+        for copied_card_id in self.game_id + 1..self.game_id + 1 + matching_numbers {
+            if let Some(copied_card) = game_card_table.get(copied_card_id) {
+                cards_won += copied_card.num_cards_won(game_card_table);
+            }
+        }
+        cards_won
     }
 }
 
@@ -86,10 +102,23 @@ fn part1(input: &str) -> u32 {
     let mut points = 0u32;
     for line in input.lines() {
         if let Ok(game) = GameCard::from_str(line) {
-            points += game.points();
+            points += game.score().0;
         }
     }
     points
+}
+
+
+fn part2(input: &str) -> usize {
+    let mut game_card_table = vec![GameCard {game_id:0, winning_numbers: vec![], your_numbers: vec![]}];
+    for line in input.lines() {
+        game_card_table.push(GameCard::from_str(line).expect("bad game card"));
+    }
+    let mut num_cards = 0;
+    for game_card in &game_card_table {
+        num_cards += game_card.num_cards_won(&game_card_table);
+    }
+    num_cards + game_card_table.len() - 1
 }
 
 
@@ -106,5 +135,6 @@ Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
 Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
         assert_eq!(part1(input), 13);
+        assert_eq!(part2(input), 30);
     }
 }
