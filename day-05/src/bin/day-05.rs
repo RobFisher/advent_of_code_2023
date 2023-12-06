781b831e92bf9343e0c89ca02a0d8e94ff76ac40
Rob Fisher <robfisher@gmail.com>
2023-12-06 03:01:54 +0000

    Day 5 part 2.

--- day-05/src/bin/day-05.rs
+++ day-05/src/bin/day-05.rs
@@ -2,7 +2,9 @@ use std::str::FromStr;
 
 fn main() {
     let input = include_str!("../input.txt");
-    println!("Part 1: {}", part1(input));
+    let (part1, part2) = both_parts(input);
+    println!("Part 1: {}", part1);
+    println!("Part 2: {}", part2);
     //println!("Part 2: {}", part2(input));
 }
 
@@ -69,9 +71,7 @@ fn traverse_maps(maps: &Vec<GardeningMap>, seed: u64) -> u64 {
 }
 
 
-fn part1(input: &str) -> u64 {
-    let mut lines = input.lines();
-    let seeds = parse_number_list_str(lines.next().unwrap()).unwrap();
+fn parse_maps(lines: std::str::Lines<'_>) -> Vec<Vec<GardeningMapEntry>> {
     let mut maps: Vec<GardeningMap> = vec![];
     let mut current_map: GardeningMap = vec![];
     for line in lines {
@@ -84,11 +84,55 @@ fn part1(input: &str) -> u64 {
             }
         }
     }
-    maps.push(current_map); // there's no line with a ':' at the end to add the final map, so we need this here
+    maps.push(current_map);
+    // there's no line with a ':' at the end to add the final map, so we need this here
     //dbg!(&maps);
-    seeds.into_iter()
-         .map(|s| traverse_maps(&maps, s))
-         .min().unwrap()
+    maps
+}
+
+
+fn expand_seeds(unexpanded_seeds: &Vec<u64>) -> Vec<u64> {
+    let mut seeds = vec![];
+    for i in 0..unexpanded_seeds.len()/2 {
+        let start_seed = unexpanded_seeds[i*2];
+        let range_length = unexpanded_seeds[(i*2)+1];
+        let end_seed = start_seed + range_length;
+        let mut seeds_to_add: Vec<u64> = (start_seed..end_seed).collect();
+        seeds.append(&mut seeds_to_add);
+    }
+    seeds
+}
+
+
+fn both_parts(input: &str) -> (u64, u64) {
+    let mut lines = input.lines();
+    let seeds = parse_number_list_str(lines.next().unwrap()).unwrap();
+    let expanded_seeds = expand_seeds(&seeds);
+    let seeds_to_search = expanded_seeds.len();
+    println!("Seeds to search: {}", seeds_to_search);
+    let maps = parse_maps(lines);
+
+    let part1_result = 
+    (&seeds).into_iter()
+            .map(|s| traverse_maps(&maps, *s))
+            .min().unwrap();
+
+    let update_interval = (seeds_to_search/1000) + 1;
+    let mut next_progress_update = update_interval;
+    let mut seeds_searched = 0;
+    let mut part2_result = u64::MAX;
+    for seed in expanded_seeds {
+        seeds_searched += 1;
+        if seeds_searched > next_progress_update {
+            next_progress_update += update_interval;
+            println!("{}%", (seeds_searched as f64 /seeds_to_search as f64)*100.0);
+        }
+        let location = traverse_maps(&maps, seed);
+        if location < part2_result {
+            part2_result = location;
+        }
+    }
+    (part1_result, part2_result)
 }
 
 
@@ -131,7 +175,7 @@ temperature-to-humidity map:
 humidity-to-location map:
 60 56 37
 56 93 4";
-        assert_eq!(part1(input), 35);
+        assert_eq!(both_parts(input), (35, 46));
         //assert_eq!(part2(input), 30);
     }
 }
