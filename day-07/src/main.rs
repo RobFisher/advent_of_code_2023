e594c7c9a8c2eec6de98df2851879812a0962886
Rob Fisher <robfisher@gmail.com>
2023-12-12 01:55:03 +0000

    Day 7 part 2.

--- day-07/src/main.rs
+++ day-07/src/main.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 use std::usize;
 use std::str::FromStr;
 use std::cmp::Ordering;
+use itertools::Itertools;
 
 
 fn main() {
@@ -15,8 +16,8 @@ fn main() {
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
 enum CamelCard {
-    C2, C3, C4, C5, C6, C7, C8, C9, C10,
-    J, Q, K, A, Joker
+    Joker, C2, C3, C4, C5, C6, C7, C8, C9, C10,
+    J, Q, K, A, None
 }
 
 
@@ -39,13 +40,13 @@ impl From<char> for CamelCard {
             'Q' => CamelCard::Q,
             'K' => CamelCard::K,
             'A' => CamelCard::A,
-            _ => CamelCard::Joker,
+            _ => CamelCard::None,
         }
     }
 }
 
 
-#[derive(PartialEq, PartialOrd)]
+#[derive(PartialEq, PartialOrd, Debug)]
 enum CamelCardsHandType {
     HighCard,
     Pair,
@@ -73,10 +74,29 @@ impl CamelCardsHand {
     }
 
     fn hand_type(&self) -> CamelCardsHandType {
-        let counts = self.card_counts();
+        let mut counts = self.card_counts();
         let mut count_counts = [0u8; 5];
+        let num_jokers = *counts.get(&CamelCard::Joker).unwrap_or(&0u8);
+        let best_card_with_most = if num_jokers > 0  && num_jokers < 5 {
+            let sorted_keys = counts.keys().sorted();
+            // find the highest non-joker card (last in the list of sorted keys) with the most copies
+            *sorted_keys.filter(|k| **k != CamelCard::Joker)
+                .max_by_key(|k|
+                    counts.get(k).unwrap_or(&0u8)
+                )
+                .unwrap_or(&CamelCard::None)
+        } else {
+            // either we don't have any jokers or there are no jokers
+            // so we don't need to find a card for jokers to become
+            CamelCard::None
+        };
+        // turn the jokers into the best card we have the most of
+        counts.remove(&CamelCard::Joker);
+        *counts.entry(best_card_with_most).or_insert(0) += num_jokers;
         for v in counts.values() {
-            count_counts[*v as usize -1] += 1;
+            if v > &0 {
+                count_counts[*v as usize -1] += 1;
+            }
         }
         match count_counts {
             [0, 0, 0, 0, 1] => CamelCardsHandType::FiveOfAKind,
@@ -129,12 +149,12 @@ struct CamelCardsHandError;
 impl FromStr for CamelCardsHand {
     type Err = CamelCardsHandError;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut hand_array = [CamelCard::Joker; 5];
+        let mut hand_array = [CamelCard::None; 5];
         let mut i = 0;
         for c in s.chars() {
             hand_array[i] = c.into();
             i += 1;
-            if i > 4 || hand_array[i-1] == CamelCard::Joker {
+            if i > 4 || hand_array[i-1] == CamelCard::None {
                 break;
             }
         }
@@ -145,9 +165,6 @@ impl FromStr for CamelCardsHand {
         }
     }
 }
-
-
-// TODO impl a type to represent a hand plus a bid
 
 
 #[derive(Debug)]
@@ -183,22 +200,55 @@ impl FromStr for CamelCardsHandListEntry {
 }
 
 
-fn both_parts(input: &str) -> (u64, u64) {
+// parse as normal and then replace the Jacks with Jokers
+impl CamelCardsHandListEntry {
+    fn from_str_with_jokers(s: &str) -> Result<Self, CamelCardsHandListEntryError> {
+        let mut hand_list_entry: CamelCardsHandListEntry = s.parse()?;
+        for i in 0..5 {
+            if hand_list_entry.hand.hand[i] == CamelCard::J {
+                hand_list_entry.hand.hand[i] = CamelCard::Joker;
+            }
+        }
+        Ok(hand_list_entry)
+    }
+}
+
+
+fn parse_input(input: &str, jokers: bool) -> Vec<CamelCardsHandListEntry> {
     let mut hand_list: Vec<CamelCardsHandListEntry>  = vec![];
     for line in input.lines() {
-        if let Ok(hand_list_entry) = line.parse() {
+        if let Ok(hand_list_entry) = if jokers {
+            CamelCardsHandListEntry::from_str_with_jokers(line)
+        } else {
+            line.parse()
+        }
+        {
             hand_list.push(hand_list_entry);
         } else {
             println!("Unable to parse {}", line);
         }
     }
+    hand_list
+}
+
+
+fn calculate_result(mut hand_list: Vec<CamelCardsHandListEntry>) -> u64 {
+    let mut result = 0;
     hand_list.sort_by_key(|h| h.hand);
-    let mut part1_result = 0;
     for (i, h) in hand_list.into_iter().enumerate() {
         //dbg!(&h);
-        part1_result += (i+1) * h.bid as usize;
+        result += (i+1) * h.bid as usize;
     }
-    let part2_result = 0;
+    result as u64
+}
+
+
+fn both_parts(input: &str) -> (u64, u64) {
+    let hand_list = parse_input(input, false);
+    let part1_result = calculate_result(hand_list);
+    println!("*****");
+    let hand_list = parse_input(input, true);
+    let part2_result = calculate_result(hand_list);
     (part1_result as u64, part2_result)
 }
 
@@ -214,7 +264,7 @@ T55J5 684
 KK677 28
 KTJJT 220
 QQQJA 483";
-        assert_eq!(both_parts(input), (6440, 0));
+        assert_eq!(both_parts(input), (6440, 5905));
         //assert_eq!(part2(input), 30);
     }
 }
